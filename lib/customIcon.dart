f054e987458b79586582811e1cbf064dce3379b2
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-03-23 18:48:48 +0100

    ProductCard file and class added

--- lib/customIcon.dart
+++ lib/customIcon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomIcon {
+class CustomIcons {
   static const IconData favorite = IconData(0xe900, fontFamily: "CustomIcon");
   static const IconData search = IconData(0xe901, fontFamily: "CustomIcon");
   static const IconData cart = IconData(0xe903, fontFamily: "CustomIcon");
--- lib/productCard.dart
+++ lib/productCard.dart
@@ -1,40 +1,18 @@
 import 'package:flutter/material.dart';
 import './customIcon.dart';
 
-void main() => runApp(MaterialApp(
-      home: MyApp(),
-      debugShowCheckedModeBanner: false,
-    ));
+class ProductCard extends StatelessWidget {
+  int cardColor;
+  String imgUrl;
+  String title;
+  String previousPrice;
+  String price;
 
-class MyApp extends StatefulWidget {
-  final Widget child;
+  ProductCard({this.cardColor, this.imgUrl, this.title, this.previousPrice, this.price});
 
-  MyApp({Key key, this.child}) : super(key: key);
-
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 30.0),
-        child: Column(
-          children: <Widget>[
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: EdgeInsets.only(top: 30.0, bottom: 25.0),
-                child: Image.asset(
-                  "assets/logo.png",
-                  width: 52.0,
-                  height: 33.0,
-                ),
-              ),
-            ),
-            Container(
+    return Container(
               width: double.infinity,
               height: 270.0,
               decoration: BoxDecoration(
@@ -86,10 +64,6 @@ class _MyAppState extends State<MyApp> {
                   )
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            );
   }
 }
